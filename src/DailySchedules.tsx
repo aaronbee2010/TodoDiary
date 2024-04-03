1446531fb1a29137b5b9b026f7f28947e8be3464
Aaron Ballagan <aaronbee2010@live.co.uk>
2024-04-03 14:00:36 +0100

    Implement routing

---
+++ src/DailySchedules.tsx
@@ -0,0 +1,9 @@
+import NavBar from './components/NavBar';
+
+const DailySchedules = (): JSX.Element => {
+    return (
+        <NavBar />
+    );
+}
+
+export default DailySchedules;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './assets/css/main.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import NavBarDesktop from './components/NavBarDesktop';
+import NavBarMobile from './components/NavBarMobile';
+
+const router = createBrowserRouter([
+    { path: "/", element: <NavBar />, },
+    { path: "/desktop", element: <NavBarDesktop />, },
+    { path: "/mobile", element: <NavBarMobile />, },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 )
